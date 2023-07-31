068bea04c2c4a66deec1d6526869a1bb1fdc4f57
eliotburk <74092915+eliotburk@users.noreply.github.com>
2023-07-31 03:25:58 -0700

    Delete script10.js

--- script10.js
+++
@@ -1,7 +0,0 @@
-window.onload = choosePic;
-
-var myPix = new Array("inco (1).jpg","inco (2).jpg"","inco (3).jpg"");
-
-function choosePic() {
-     var randomNum = Math.floor(Math.random() * myPix.length);
-     document.getElementById("myPicture").src = myPix[randomNum];
